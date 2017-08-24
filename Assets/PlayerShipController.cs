25dcd2295cef4da7e0315d207f576984115c2d79
Paolo <chiste.paolo.securemail@gmail.com>
2017-08-24 11:47:54 +0200

    Inizio modifiche per utilizzo rigidbody2D

--- Assets/PlayerShipController.cs
+++ Assets/PlayerShipController.cs
@@ -19,42 +19,35 @@ public class PlayerShipController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float a = accelleration * Time.deltaTime;
+		Vector2 accellerationVector = transform.right * accelleration;
 		Rigidbody2D shipBody = this.GetComponent<Rigidbody2D> ();
 
 		//utilizza transform.right dato che la trasformata ha rotazione iniziale di 0,0,90
 
 		if(Input.GetKey(KeyCode.W)){
-			shipBody.velocity += new Vector2(transform.right.x,transform.right.y) * a;
-		}else{
-			shipBody.velocity -= new Vector2 (transform.right.x, transform.right.y) * (0.3f * Time.deltaTime);
+			shipBody.AddForce (accellerationVector);
 		}
 		if(Input.GetKey(KeyCode.S)){
-			shipBody.velocity -= new Vector2(transform.right.x,transform.right.y) * a;
+			shipBody.AddForce (-accellerationVector);
 		}
 		if(Input.GetKey(KeyCode.A)){
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				//smokeTrails.Play ();
-				shipBody.rotation += speedTurningRadius(shipBody.velocity.magnitude) + 1.5f;
+				shipBody.AddTorque(0.2f);
 			}else{
-				shipBody.rotation += speedTurningRadius(shipBody.velocity.magnitude);
+				shipBody.AddTorque(0.1f);
 			}
 		}
 		if(Input.GetKey(KeyCode.D)){
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				//smokeTrails.Play ();
-				shipBody.rotation -= speedTurningRadius(shipBody.velocity.magnitude) + 1.5f;
+				shipBody.AddTorque(-0.2f);
 			}else{
-				shipBody.rotation -= speedTurningRadius(shipBody.velocity.magnitude);
+				shipBody.AddTorque(-0.1f);
 			}
 		}
 		shipBody.velocity = transform.right * Mathf.Clamp (shipBody.velocity.magnitude, minimumSpeed, maximumSpeed);
 
 		Camera.main.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -10f);
-	}
-
-	float speedTurningRadius(float speed){
-		Debug.Log (8f * (1f / Mathf.Clamp (speed, 1.0f, maximumSpeed))); 
-		return 8f * 1f / Mathf.Clamp(speed, 1.0f,maximumSpeed);
 	}
 }
