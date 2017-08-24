b239c67686d5dee3590fc58a5487e39f6c5714b5
Paolo <chiste.paolo.securemail@gmail.com>
2017-08-24 11:53:01 +0200

    Refactoring

--- Assets/PlayerShipController.cs
+++ Assets/PlayerShipController.cs
@@ -8,6 +8,7 @@ public class PlayerShipController : MonoBehaviour {
 	public float maximumSpeed = 10f;
 	public float health = 100f;
 	public float minimumSpeed = -7f;
+	public float turningRadius = 0.5f;
 
 	private ParticleSystem smokeTrails;
 
@@ -33,17 +34,17 @@ public class PlayerShipController : MonoBehaviour {
 		if(Input.GetKey(KeyCode.A)){
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				//smokeTrails.Play ();
-				shipBody.AddTorque(0.2f);
+				shipBody.AddTorque(turningRadius);
 			}else{
-				shipBody.AddTorque(0.1f);
+				shipBody.AddTorque(turningRadius);
 			}
 		}
 		if(Input.GetKey(KeyCode.D)){
 			if (Input.GetKey (KeyCode.LeftShift)) {
 				//smokeTrails.Play ();
-				shipBody.AddTorque(-0.2f);
+				shipBody.AddTorque(-turningRadius);
 			}else{
-				shipBody.AddTorque(-0.1f);
+				shipBody.AddTorque(-turningRadius);
 			}
 		}
 		shipBody.velocity = transform.right * Mathf.Clamp (shipBody.velocity.magnitude, minimumSpeed, maximumSpeed);
