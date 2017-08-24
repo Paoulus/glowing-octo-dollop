3824c057fe53f946b3b2999335b5aa70eacb17a8
MaxFrax <maxfrax@gmail.com>
2017-08-24 16:55:30 +0200

    Little code refactor

---
+++ Assets/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+  public GameObject characterToFollow;
+
+  void LateUpdate () {
+    transform.position = new Vector3 (characterToFollow.transform.position.x, characterToFollow.transform.position.y, -10f);
+  }
+}
--- Assets/PlayerCarController.cs
+++ Assets/PlayerCarController.cs
@@ -2,74 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (ParticleSystem))]
 public class PlayerCarController : MonoBehaviour {
 
-	public float accelleration = 0.5f;
-	public float maximumSpeed = 10f;
-	public float health = 100f;
-	public float minimumSpeed = -7f;
-	public float turningRadius = 0.5f;
-	public float driftQuantity = 1.4f;
+  public float accelleration = 0.5f;
+  public float maximumSpeed = 10f;
+  public float health = 100f;
+  public float minimumSpeed = -7f;
+  public float turningRadius = 0.5f;
+  public float driftQuantity = 1.4f;
 
-	public ParticleSystem explosionEffect;
+  public ParticleSystem explosionEffect;
 
-	private ParticleSystem smokeTrails;
+  private ParticleSystem smokeTrails;
 
-	void OnCollisionEnter2D(Collision2D coll){
-		health -= 20f;
-		if(health <= 0){
-			explosionEffect.transform.position = coll.contacts[0].point;
-			explosionEffect.Play ();
-			Destroy (this.gameObject);
-		}
-	}
+  void OnCollisionEnter2D (Collision2D coll) {
+    health -= 20f;
+    if (health <= 0) {
+      explosionEffect.transform.position = coll.contacts[0].point;
+      explosionEffect.Play ();
+      Destroy (this.gameObject);
+    }
+  }
 
-	// Use this for initialization
-	void Start () {
-		smokeTrails = GetComponent<ParticleSystem> ();
-	}
+  // Use this for initialization
+  void Start () {
+    smokeTrails = GetComponent<ParticleSystem> ();
+  }
 
-	// Update is called once per frame
-	void Update () {
-		Vector2 accellerationVector = transform.up * accelleration;
-		Rigidbody2D shipBody = this.GetComponent<Rigidbody2D> ();
+  // Update is called once per frame
+  void Update () {
+    Vector2 accellerationVector = transform.up * accelleration;
+    Rigidbody2D shipBody = this.GetComponent<Rigidbody2D> ();
 
-		//utilizza transform.right dato che la trasformata ha rotazione iniziale di 0,0,90
+    //utilizza transform.right dato che la trasformata ha rotazione iniziale di 0,0,90
 
-		if(Input.GetKey(KeyCode.W)){
-			shipBody.AddForce (accellerationVector);
-		}
-		if(Input.GetKey(KeyCode.S)){
-			shipBody.AddForce (-accellerationVector);
-		}
+    if (Input.GetKey (KeyCode.W)) {
+      shipBody.AddForce (accellerationVector);
+    }
+    if (Input.GetKey (KeyCode.S)) {
+      shipBody.AddForce (-accellerationVector);
+    }
 
+    if (Input.GetKey (KeyCode.A)) {
+      if (Input.GetKey (KeyCode.LeftShift)) {
+        shipBody.AddTorque (turningRadius * driftQuantity);
+      } else {
+        shipBody.AddTorque (turningRadius);
+      }
+    }
+    if (Input.GetKey (KeyCode.D)) {
+      if (Input.GetKey (KeyCode.LeftShift)) {
+        shipBody.AddTorque (-turningRadius * driftQuantity);
+      } else {
+        shipBody.AddTorque (-turningRadius);
+      }
+    }
 
-		if(Input.GetKey(KeyCode.A)){
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				shipBody.AddTorque(turningRadius *driftQuantity);
-			}else{
-				shipBody.AddTorque(turningRadius);
-			}
-		}
-		if(Input.GetKey(KeyCode.D)){
-			if (Input.GetKey (KeyCode.LeftShift)) {
-				shipBody.AddTorque(-turningRadius * driftQuantity);
-			}else{
-				shipBody.AddTorque(-turningRadius);
-			}
-		}
-
-		if((Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.S))) && Input.GetKey(KeyCode.LeftShift)){
-			if (!smokeTrails.isPlaying) {
-				smokeTrails.Play ();
-			}
-		}else{
-			if(smokeTrails.isPlaying)
-				smokeTrails.Stop ();
-		}
-
-		shipBody.velocity = transform.up * Mathf.Clamp (shipBody.velocity.magnitude, minimumSpeed, maximumSpeed);
-
-		Camera.main.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -10f);
-	}
+    if ((Input.GetKey (KeyCode.A) || (Input.GetKey (KeyCode.S))) && Input.GetKey (KeyCode.LeftShift)) {
+      if (!smokeTrails.isPlaying) {
+        smokeTrails.Play ();
+      }
+    } else {
+      if (smokeTrails.isPlaying)
+        smokeTrails.Stop ();
+    }
+  }
 }
