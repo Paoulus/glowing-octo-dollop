bb7172c7ba24e07d30747a020c2711139f6ed31f
Paolo <chiste.paolo.securemail@gmail.com>
2017-08-25 14:08:10 +0200

    Inizio refactoring classe PlayerCar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-
-	private PlayerCarController currentPlayer;
+	
 	private LevelManager mainLevelManager;
 
 	public ParticleSystem explosionEffect;
 
 	public Text indicatoreGiri;
+
+	public PlayerCarController[] players;
 
 	public int maximumLoops = 3;
 	private int loops = 0;
@@ -26,20 +27,21 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		currentPlayer = FindObjectOfType<PlayerCarController> () ;
 		mainLevelManager = FindObjectOfType<LevelManager> ();
 		timer = FindObjectOfType<Cronometro> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(currentPlayer.totalHealth <= 0 && currentPlayer && !explosionEffect.isPlaying){
-			Destroy (currentPlayer.gameObject);
-			explosionEffect.transform.position = currentPlayer.transform.position;
-			explosionEffect.Play ();
-			Invoke ("GameOver", 2.8f);
+		foreach (PlayerCarController currentPlayer in players) {
+			if (currentPlayer.totalHealth <= 0 && currentPlayer && !explosionEffect.isPlaying) {
+				Destroy (currentPlayer.gameObject);
+				explosionEffect.transform.position = currentPlayer.transform.position;
+				explosionEffect.Play ();
+				Invoke ("GameOver", 2.1f);
+			}
+			indicatoreGiri.text = loops.ToString () + " / " + maximumLoops.ToString ();
 		}
-		indicatoreGiri.text = loops.ToString() + " / " + maximumLoops.ToString();
 	}
 
 	public void PassingStartLine(){
