aa97c07148318f6b1869dc75c707b1c46a4f22ff
Paolo <chiste.paolo.securemail@gmail.com>
2017-09-01 09:11:25 +0200

    Spostati gli script nella cartella corretta.

--- Assets/Scripts/StartLine.cs
+++ Assets/Scripts/StartLine.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StartLine : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D collider){
 		LoopsComponent l = collider.gameObject.GetComponent<LoopsComponent>();
+		Cronometro c = collider.gameObject.GetComponent<Cronometro>();
 		if(l){
 			l.IncreaseLoops();
 		}
